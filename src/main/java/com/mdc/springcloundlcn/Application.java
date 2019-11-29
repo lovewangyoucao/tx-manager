f8d18f51dcd654e1517520ab1a2636dc70169114
lovewangyoucao <845676029@qq.com>
2019-11-30 01:31:32 +0800

    Initial commit

---
+++ src/main/java/com/mdc/springcloundlcn/Application.java
@@ -0,0 +1,13 @@
+package com.mdc.springcloundlcn;
+
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableTransactionManagerServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
